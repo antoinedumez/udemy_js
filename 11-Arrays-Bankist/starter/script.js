99040e66f77fd89f20d521020ab392722dcd61ff
Antoine Dumez <MY_NAME@example.com>
2022-10-21 16:34:44 +0200

    ok

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -84,30 +84,52 @@ accounts.forEach(user => {
     .map(val => val[0])
     .join('');
 });
-// Login and call function to display infos
+// Give current account global scope
 let currentAccount;
+// Login and call function to display infos
 btnLogin.addEventListener('click', ev => {
   ev.preventDefault();
+  // Log
   accounts.find(el => {
     if (
       inputLoginUsername.value === el.username &&
       Number(inputLoginPin.value) === el.pin
     ) {
+      currentAccount = el;
       // Say Hello to the user
       labelWelcome.textContent = `Hello ${el.owner}`;
       // Display the screen
       containerApp.style.opacity = 100;
-      // Display all operations
-      el.movements.forEach((mov, index) => displayOperations(mov, index));
       // Display all infos about user
       calcDisplaySummary(el);
       // Clean inputs fields
       inputLoginUsername.value = inputLoginPin.value = '';
       inputLoginPin.blur();
-
-      console.log(el);
     }
   });
+});
+
+// Transfert monney
+btnTransfer.addEventListener('click', ev => {
+  ev.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  if (
+    receiverAcc &&
+    receiverAcc !== currentAccount &&
+    amount > 0 &&
+    amount <= currentAccount.balance
+  ) {
+    // Add transfert to users
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+    // Re caclculate and display infos
+    calcDisplaySummary(currentAccount);
+    // Clean the transfer inputs
+    inputTransferAmount.value = inputTransferTo.value = '';
+  }
 });
 // Display all operations (work with a foreach)
 const displayOperations = function (mov, index) {
@@ -123,9 +145,11 @@ const displayOperations = function (mov, index) {
 };
 //Calc and display all sum
 const calcDisplaySummary = function (el) {
-  // Display Current balance
-  const currentBalance = el.movements.reduce((acc, opp) => acc + opp, 0);
-  labelBalance.textContent = `${currentBalance} 🤑`;
+  // Display all operations
+  el.movements.forEach((mov, index) => displayOperations(mov, index));
+  // Display Current balance and save to the current account
+  el.balance = el.movements.reduce((acc, opp) => acc + opp, 0);
+  labelBalance.textContent = `${el.balance} 🤑`;
   // Calcul and display in
   const sumIn = el.movements
     .filter(value => value > 0)
@@ -138,9 +162,8 @@ const calcDisplaySummary = function (el) {
   labelSumOut.textContent = `${-sumOut} 😬`;
   const interest = el.movements
     .filter(mov => mov > 0)
-    .map(deposit => (deposit * 1.2) / 100)
+    .map(deposit => (deposit * el.interestRate) / 100)
     .filter((int, i, arr) => {
-      // console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
@@ -246,3 +269,71 @@ const calcAverageHumanAge = dogsAges =>
 
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 */
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+1. Loop over the array containing dog objects, and for each dog, 
+calculate the recommended food portion and add it to the object as a new property. 
+Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. 
+(The result is in grams of food, and the weight needs to be in kg)
+
+2. Find Sarah's dog and log to the console whether it's eating too much or too little.
+ HINT: Some dogs have multiple owners, s
+ o you first need to find Sarah in the owners array, 
+ and so this one is a bit tricky (on purpose) 🤓
+
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') 
+and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3.,
+ like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+ 5. Log to the console whether there is any dog eating 
+EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+GOOD LUCK 😀
+*/
+
+const dogs = [
+  { weight: 22, curFood: 284, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+// calculate recom food
+dogs.forEach(
+  dog => (dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28))
+);
+// is sarah's dog eat too much ?
+// const sarahDog = dogs.find(dog => dog.owners.includes('Sarah'));
+// sarahDog.curFood > sarahDog.recommendedFood
+//   ? console.log('too much')
+//   : console.log('ok');
+// arrays dog too much and too little
+
+const toomuch = dogs
+  .filter(dog => dog.curFood > dog.recommendedFood * 0.9)
+  .flatMap(dog => dog.owners);
+
+const message = `${toomuch.join(' and ')} eat too much`;
+const ok = dogs.some(dog => dog.curFood === dog.recommendedFood);
+console.log(dogs, ok);
